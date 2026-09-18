065a72ed42b2fb3669e4eab7fad5a4331eeaf56d
EmiBuffet <45399757+EmiBuffet@users.noreply.github.com>
2026-09-18 18:03:19 -0300

    Disable development checks for API mapping
    Comment out development environment checks for API mapping.

--- 2026/FacturasAPI/Program.cs
+++ 2026/FacturasAPI/Program.cs
@@ -20,11 +20,11 @@ builder.Services.AddScoped<IFacturasLogica, FacturasLogica>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.MapOpenApi();
     app.MapScalarApiReference();
-}
+//}
 
 app.UseHttpsRedirection();
 
